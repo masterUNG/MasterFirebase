34379ad180a137cdfd732a67fad26b4594828895
masterUNG <phrombutr@gmail.com>
2019-04-29 15:32:49 +0700

    Color Gradient

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,17 +6,39 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showAppName() {
+    return Text(
+      'Master Flutter',
+      style: TextStyle(
+          fontFamily: 'Schoolbell',
+          fontSize: 35.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.red[900]),
+    );
+  }
 
-  Widget showAppName(){
-    return Text('Master Flutter');
+  Widget showLogo() {
+    return Image.asset('images/logo.png');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(padding: EdgeInsets.only(top: 100.0),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.blue, Colors.white],begin: Alignment(-1, -1))),
+        padding: EdgeInsets.only(top: 100.0),
         alignment: Alignment(0, -1),
-        child: showAppName(),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 200.0,
+              height: 200.0,
+              child: showLogo(),
+            ),
+            Container(margin: EdgeInsets.only(top: 15.0), child: showAppName())
+          ],
+        ),
       ),
     );
   }
