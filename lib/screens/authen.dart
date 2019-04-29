7172e7c460a1dbb2e6defa351496d8fcce056980
masterUNG <phrombutr@gmail.com>
2019-04-29 16:36:33 +0700

    Finish 29April

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,6 +6,38 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget signUpButton() {
+    return RaisedButton(color: Colors.orange[200],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      child: Text('Sign Up'),
+      onPressed: () {},
+    );
+  }
+
+  Widget signInButton() {
+    return RaisedButton(
+      color: Colors.orange,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      child: Text('Sign In'),
+      onPressed: () {},
+    );
+  }
+
+  Widget passwordTextFormField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+          labelText: 'Password :', hintText: 'More 6 Charactor'),
+    );
+  }
+
+  Widget emailTextFormField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'Email :', hintText: 'you@email.com'),
+    );
+  }
+
   Widget showAppName() {
     return Text(
       'Master Flutter',
@@ -24,9 +56,11 @@ class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: Container(
         decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [Colors.blue, Colors.white],begin: Alignment(-1, -1))),
+            gradient: LinearGradient(
+                colors: [Colors.blue, Colors.white], begin: Alignment(-1, -1))),
         padding: EdgeInsets.only(top: 100.0),
         alignment: Alignment(0, -1),
         child: Column(
@@ -36,7 +70,31 @@ class _AuthenState extends State<Authen> {
               height: 200.0,
               child: showLogo(),
             ),
-            Container(margin: EdgeInsets.only(top: 15.0), child: showAppName())
+            Container(margin: EdgeInsets.only(top: 15.0), child: showAppName()),
+            Container(
+              margin: EdgeInsets.only(left: 50.0, right: 50.0),
+              child: emailTextFormField(),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 50.0, right: 50.0),
+              child: passwordTextFormField(),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 15.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(right: 4.0),
+                      child: signInButton(),
+                    ),
+                  ),
+                  Expanded(
+                    child: signUpButton(),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
