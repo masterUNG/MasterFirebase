147f70efbce46324b2845715af450864da5915b3
masterUNG <phrombutr@gmail.com>
2019-04-30 10:57:27 +0700

    updat

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,10 +6,48 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  Widget passwordTextFormField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          icon: Icon(
+            Icons.lock,
+            color: Colors.blue,
+          ),
+          labelText: 'Password :',
+          hintText: 'More 6 Charactor',
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15.0),
+              borderSide: BorderSide(width: 1.0, color: Colors.grey))),
+    );
+  }
+
+  Widget emailTextFormField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15.0),
+              borderSide: BorderSide(width: 1.0, color: Colors.grey)),
+          labelText: 'Email :',
+          hintText: 'you@email.com',
+          icon: Icon(
+            Icons.email,
+            color: Colors.green,
+          )),
+    );
+  }
+
   Widget nameTextFormField() {
     return TextFormField(
-      decoration:
-          InputDecoration(labelText: 'Name', hintText: 'Type Your Name'),
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15.0),
+              borderSide: BorderSide(width: 1.0, color: Colors.grey)),
+          labelText: 'Name :',
+          hintText: 'Type Your Name',
+          icon: Icon(
+            Icons.face,
+            color: Colors.red,
+          )),
     );
   }
 
@@ -19,9 +57,16 @@ class _RegisterState extends State<Register> {
       appBar: AppBar(
         title: Text('Register'),
       ),
-      body: Container(padding: EdgeInsets.all(50.0),
+      body: Container(
+        padding: EdgeInsets.all(50.0),
         child: Column(
-          children: <Widget>[nameTextFormField()],
+          children: <Widget>[
+            nameTextFormField(),
+            Container(margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: emailTextFormField(),
+            ),
+            passwordTextFormField()
+          ],
         ),
       ),
     );
